e3d96c566f2f76c936fca068ffe87708c1e4524a
allanbez <160733709+allanbez@users.noreply.github.com>
2024-03-14 14:15:43 +0000

    criando o arquivo de operadores aritmeticos

---
+++ operadores-arítmeticos.js
@@ -0,0 +1,11 @@
+const numero1 =40;
+const numero2 =20;
+
+
+
+console.log(`os numeros das operaçoes sao ${numero1} e ${numero2}`)
+console.log(`a soma dos números é ${numero1+numero2}`);
+console.log(`a subtração dos números é ${numero1+numero2}`);
+console.log(`a multiplicação dos números é ${numero1+numero2}`);
+console.log(`a divisao dos números é ${numero1+numero2}`);
+console.log(`o resto dos números é ${numero1+numero2}`);
