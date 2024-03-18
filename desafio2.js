cadd9d38b7f106d9f0e95b4843e1d5ce12592e69
allanbez <160733709+allanbez@users.noreply.github.com>
2024-03-18 14:45:44 +0000

    nao consigo mais

--- desafio2.js
+++ desafio2.js
@@ -1,21 +1,23 @@
-const  peso = window.prompt ('digite seu peso')
-const altura = window.prompt('digite sua altura')
-const media= peso /(altura **2);
-if (media <= 18,5){
-console.log ('abaixo do peso')
+const
+  peso = window.prompt ('digite seu peso'),
+  altura = window.prompt('digite sua altura'),
+ media= peso /(altura **2);
+ 
+if (media <= 18.5){
+console.log (`abaixo do peso valor do imc ${media.toFixed(2)}`)
 }
-else if (media  <=24,9){
-    window.alert('peso normal')
+else if (media  <=24.9){
+    window.alert(`peso normal valor do imc ${media.toFixed(2)}`)
 }
-else if (media <=29,9){
-    window.alert ('sobrepeso')
+else if (media <=29.9){
+    window.alert (`sobrepeso valor do imc ${media.toFixed(2)}`)
 }
-else if (media <=34,9){
-    window.alert('obesidade grau 1')
+else if (media <=34.9){
+    window.alert(`obesidade grau 1 valor do imc ${media.toFixed(2)}`)
 }
 else if(media <= 39.9){
-    window.alert ('obesidade grau 2')
+    window.alert (`obesidade grau 2 valor do imc ${media.toFixed(2)}`)
 }
 else {
-    window.alert('obesidade grau 3')
+    window.alert(`obesidade grau 3 valor do imc ${media.toFixed(2)}`)
 }
---
+++ desafio3.js
@@ -0,0 +1,12 @@
+const
+graus=window.prompt ('digite a temperatura'),
+
+celsius=tc ,
+fahrenheint= tf,
+kelvin= tk,
+celsiusfahrenheint= tf = (tc*9/5) +32,
+celsiuskevin= tk = tc-273.15,
+fahrenheintcelsius=tc = (tf + 459.67)*5/9,
+kelvincelsius= tc =tk + 273.15,
+kelvinfahreheint= tf =tk * 9/5-459.67;
+
