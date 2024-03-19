8c03b4630114bd0140decc07ec94228ff01089c3
allanbez <160733709+allanbez@users.noreply.github.com>
2024-03-19 14:25:44 +0000

    finalmente consegui

--- desafio3.js
+++ desafio3.js
@@ -1,12 +1,34 @@
 const
-graus=window.prompt ('digite a temperatura'),
+temperatura = parseFloat(window.prompt ('digite a temperatura')),
+escalaatual=window.prompt ('digite a unidade de temperatura').toLowerCase(),
+convercao=window.prompt('converção para sua temperatura').toLowerCase();
 
-celsius=tc ,
-fahrenheint= tf,
-kelvin= tk,
-celsiusfahrenheint= tf = (tc*9/5) +32,
-celsiuskevin= tk = tc-273.15,
-fahrenheintcelsius=tc = (tf + 459.67)*5/9,
-kelvincelsius= tc =tk + 273.15,
-kelvinfahreheint= tf =tk * 9/5-459.67;
+ const 
+celsiusfahrenheint= (temperatura*9/5) +32,
+celsiuskelvin= temperatura + 273.15,
+fahrenheintcelsius=(temperatura - 32)*5/9,
+fahrenheintkelvin=(temperatura+459.67)*5/9,
+kelvincelsius= temperatura + 273.15,
+kelvinfahreheint = temperatura * 9/5-459.67;
 
+if(escalaatual=="c" && convercao == "f"){
+    window.alert (`temperatura =${celsiusfahrenheint}`);
+} 
+else if(escalaatual=="c"&& convercao == "k"){
+    window.alert (`temperatura=${celsiuskelvin}`);
+}
+else if (escalaatual=="f" && convercao == "c"){
+    window.alert (`temperatura=${fahrenheintcelsius}`);
+}
+else if (escalaatual== "f" && convercao =="k"){
+    window.alert (`temperatura=${fahrenheintkelvin}`);
+}
+else if (escalaatual == "k" && resultado =="c"){
+    window.alert(`temperatura${kelvincelsius}`);
+}
+else if (escalaatual == "k" && resultado == "f"){
+    window.alert (`temperatura${kelvinfahreheint}`);
+}
+else {
+    window.alert(`resultado inválido`);
+}
