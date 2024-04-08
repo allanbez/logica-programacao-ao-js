607015686eabd342c9224e58600c6f8f3884929f
allanbez <estevesallan213@gmail.com>
2024-04-08 11:50:05 -0300

    fazendo desafios

--- futebol.js
+++ futebol.js
@@ -1,3 +1,10 @@
-function soma(valor1,valor2);{
+function soma(time1,time2){
 
-}
+    if(time1 > time2) {
+        return 'Time 1 Vence'
+    }else if (time2 > time1) {
+        return 'time 2 vence'
+        }else 
+        return 'EMPATE'
+    }
+
---
+++ nome.js
@@ -0,0 +1,7 @@
+const variavel =['a','b','c','d','e','f','g','h','i','j','k'];
+
+for (let i=0; i <variavel.length; i++){
+
+ 
+    console.log(variavel[i]);
+}
