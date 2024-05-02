bed7128afcd08ea7ab1e94a69013b5032e921e73
allanbez <estevesallan213@gmail.com>
2024-05-02 11:47:03 -0300

    resolvendo desafio 5

--- operadores-arítmeticos.js
+++ operadores-arítmeticos.js
@@ -4,9 +4,15 @@ const numero2 =2;
 
 
 console.log(`os numeros das operaçoes sao ${numero1} e ${numero2}`)
+
 console.log(`a soma dos números é ${numero1+numero2}`);
+
 console.log(`a subtração dos números é ${numero1+numero2}`);
+
 console.log(`a multiplicação dos números é ${numero1+numero2}`);
+
 console.log(`a divisao dos números é ${numero1+numero2}`);
+
 console.log(`o resto dos números é ${numero1+numero2}`);
+
 console.log(`numero1 elevado a potencia do numero2 é ${numero1**numero2}`);
